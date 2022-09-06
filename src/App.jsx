e30070234151d23af6a47a50523d84f7b1bc43d3
alfons <alfonsspj@gmail.com>
2022-09-06 16:55:14 -0300

    Consultando a la api en base a Categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+// dos formas de importar
+// import Container from '@mui/material/Container';
+import { Container, Grid, Typography } from '@mui/material';
+import Formulario from './components/Formulario';
+import { NoticiasProvider } from './context/NoticiasProvider';
 
 function App() {
 
   return (
-    <>
-      <h1>Noticias</h1>
-    </>
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align='center' marginY={5} component="h1" variant='h3'>
+            Buscador de Noticias
+          </Typography>
+        </header>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
+        </Grid>
+      </Container>
+    </NoticiasProvider>
   )
 }
 
